41fc5e5271f182ea761eb709ca2ee43b268d6fe9
kekesh <0ekesh@gmail.com>
2021-03-28 18:16:34 -0400

    VRPWH: Initial commit

---
+++ heuristics/circle/generate_instance.py
@@ -0,0 +1,15 @@
+class VRPWHCircle:
+  def __init__(self, radius, num_points):
+    self.radius = radius
+    self.num_points = num_points
+
+    # Generate num_points uniformly on x^2 + y^2 = r^2.
+
+
+  def __str__(self):
+  
+
+
+
+problem = VRPWHCircle()
+print("asd")
